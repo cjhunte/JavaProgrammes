ea202dc0337ba357f00d94aa5b11a25e1790ef81
CONNOR HUNTER <60004087@nrc.ac.uk>
2023-01-31 14:12:43 +0000

    1.07

---
+++ src/blendedLearning/Pay.java
@@ -0,0 +1,20 @@
+package blendedLearning;
+
+public class Pay
+
+{
+    public static void WeeklyPay()
+    {
+        EmployeeName = "";
+
+        System.out.println("please type your name");
+        EmployeeName = keyboard.nextLine();
+
+
+    }
+
+
+
+
+
+}
