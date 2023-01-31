846c46ca0d1b9d3792c3daac9f7c1012c224f655
CONNOR HUNTER <60004087@nrc.ac.uk>
2023-01-31 13:40:21 +0000

    1.05

---
+++ src/Week1/Myfirstprogram.java
@@ -0,0 +1,38 @@
+package Week1;
+
+import java.util.Scanner;
+import java.text.*;
+
+
+public class Myfirstprogram
+{
+   public static void main (String[] args)
+   {
+      /*
+      ~String name = "rachel" ;
+      ~int age = 21 ;
+
+      ~System.out.println( "my name is " + name + " and i am "+ age + " years old");
+      */
+
+      int number1 = 14;
+      double number2 = 3;
+      double result1, result2;
+      int number3 = 14;
+      int number4 = 3;
+      int result3 ;
+      double result4;
+
+
+      result1 = number1 / number2;
+      System.out.println("result one is: " + result1 );
+      result2 = number1 % number2;
+      System.out.println("result two is: " + result2 );
+      result3 = number3/number4;
+      System.out.println("result three is: " + result3 );
+      result4 = number3/number4;
+      System.out.println("result four is: " + result4 );
+
+   }
+
+}//class
