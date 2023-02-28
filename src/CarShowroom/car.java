3934ff0e9d34ecefc607bd003bea8eea1d41aaf4
CONNOR HUNTER <60004087@nrc.ac.uk>
2023-02-28 13:46:51 +0000

    2.0 week 2 activites updated

---
+++ src/CarShowroom/car.java
@@ -0,0 +1,21 @@
+package CarShowroom;
+import java.util.Date;
+
+public class car {//creates the class for car
+
+
+        private String registration;//these define the values for car to later be changed via overloading
+        private String make;
+        private String model;
+        private int engine;
+        private String colour;
+        private int manYear;
+        private int price;
+        private Date dateRegistered ;
+
+
+
+    public car ()
+    {}//this is our constructor method again to be used later
+
+}
