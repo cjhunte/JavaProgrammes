9d022f04f7524cddec506f6f955775ceb74f6292
CONNOR HUNTER <60004087@nrc.ac.uk>
2023-03-21 12:01:33 +0000

    2.2

--- src/EstateAgents/ArrayOfProperties.java
+++ src/EstateAgents/ArrayOfProperties.java
@@ -4,73 +4,87 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 import java.util.AbstractList;
+
 public class ArrayOfProperties
 {
-      private List<property> PropertiesL;
+   private List<property> PropertiesL;
 
-      public ArrayOfProperties()
+   public ArrayOfProperties()
+{
+   PropertiesL = new ArrayList<property>();
+}//Property array.
+
+   protected property Getcurr(int position)
    {
-      PropertiesL = new ArrayList<property>();
-   }//Property array.
+      return PropertiesL.get(position);
+   }//selects choosen property
+   protected int GetNumProp()
+   {
+      return PropertiesL.size();
+   }//tells us how many properties are registered on the system
 
-      protected property Getcurr(int position)
-      {
-         return PropertiesL.get(position);
-      }//selects choosen property
-      protected int GetNumProp()
-      {
-         return PropertiesL.size();
-      }//tells us how many properties are registered on the system
+   protected String GetStreet(int position)
+   {
+      return PropertiesL.get(position).getAddReslin();
+   }//selects choosen property
 
-      protected String GetStreet(int position)
-      {
-         return PropertiesL.get(position).getAddReslin();
-      }//selects choosen property
+   protected void insert(property newproperty)
+   {
+      PropertiesL.add(newproperty);
+      System.out.println(newproperty.getfulladd() + " Added");
+   }//inserts new property
 
-      protected void insert(property newproperty)
-      {
-         PropertiesL.add(newproperty);
-         System.out.println(newproperty.getfulladd() + " Added");
-      }//inserts new property
+   protected void removeprop(int position, property delprop)
+   {
+      PropertiesL.remove(position);
+      System.out.println(delprop.getfulladd() + " Removed");
+   }//removes property selected
 
-      protected void removeprop(int position, property delprop)
-      {
-         PropertiesL.remove(position);
-         System.out.println(delprop.getfulladd() + " Removed");
-      }//removes property selected
+   protected int findPropertyByStreet(String street)
+   {
+      int index = 0;
+      boolean found = false;
+      int position = -1;
 
-      protected int findPropertyByStreet(String street)
+      do
       {
-         int index = 0;
-         boolean found = false;
-         int position = -1;
 
-         do
+      if (PropertiesL.get(index).getAddReslin() == street)
+      {
+         found = true;
+         position = index;
+      }
+      index ++;
+
+      }while(found = false && index < PropertiesL.size());
+      return position;
+   }
+
+   protected ArrayList<Integer> findPropertyByTown(String town)
+   {
+      int index = 0;
+      ArrayList<Integer> result = new ArrayList<Integer>();
+
+      for (int i=0; i< PropertiesL.size(); i++);
+      {
+         if (PropertiesL.get(index).getTown() == town)
          {
+            result.add(index);
 
-         if (PropertiesL.get(index).getAddReslin() == street)
-         {
-            found = true;
-            position = index;
          }
-         index ++;
+      }return result;
+   }
 
-         }while(found = false && index < PropertiesL.size());
-         return position;
-      }
-
-      protected ArrayList<Integer> findPropertyByTown(String town)
+   protected void displayAprop(int position)
+    {
+       PropertiesL.get(position).displaypropertydetails();
+    }
+   protected void displayAllprop()
+   {
+      for (property allProp : PropertiesL)
       {
-         int index = 0;
-         ArrayList<Integer> result = new ArrayList<Integer>();
-
-         for (int i=0; i< PropertiesL.size(); i++);
-         {
-            if (PropertiesL.get(index).getTown() == town)
-            {
-               result.add(index);
-
-            }
-         }return result;
+         allProp.displaypropertydetails();
       }
+   }
+
 }
